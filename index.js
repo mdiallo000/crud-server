f8042e2c03ab1fb4056bb1ca46d9c3f8bde0093e
mdiallo98 <mamadou1237@gmail.com>
2022-02-01 21:43:40 -0500

    db query finished

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+	user: "user",
+	host: "localhost",
+	password: "Booba98@",
+	databae: "employeregsystem",
+});
+app.post("/create", (req, res) => {
+	const name = req.body.name;
+	const age = req.body.age;
+	const country = req.body.country;
+	const position = req.body.position;
+	const wage = req.body.wage;
+});
+
 app.listen(3001, () => {
 	console.log("The server is running on Port 3001");
 });
